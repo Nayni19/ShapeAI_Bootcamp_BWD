a569da4c9545230db9e2d4f5d6ec85a4fe11c7d0
Nayni Shah <83155646+Nayni19@users.noreply.github.com>
2021-06-28 12:30:43 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React JS Bootcamp</h1>
+      <p>Basic Front End Web Dev with JavaScript and React.js</p>
+    </div>
+  );
+}
+
+export default Info;
---
+++ Info1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Info1() {
+  return (
+    <div className="note">
+      <h1>Python And CyberSecurity Bootcampt</h1>
+      <p>Basic Python with CyberSecurity</p>
+    </div>
+  );
+}
+
+export default Info1;
